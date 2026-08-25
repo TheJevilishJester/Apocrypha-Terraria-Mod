7038d0a84fca20c6c99330153528f1a1ef48e687
White space <alexgaming9983579898@gmail.com>
2026-08-25 12:47:58 -0400

    Update BambooSword.cs for smooth swing support

--- Content/Items/BambooSword.cs
+++ Content/Items/BambooSword.cs
@@ -1,38 +1,40 @@
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using ApocryphaAnimatorBranch.Content.Projectiles;
 
 namespace TheApocryphaMod.Content.Items
 {
-	// This is a basic item template.
-	// Please see tModLoader's ExampleMod for every other example:
-	// https://github.com/tModLoader/tModLoader/tree/stable/ExampleMod
-	public class BambooSword : ModItem
-	{
-		// The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.TheApocryphaMod.hjson' file.
-		public override void SetDefaults()
-		{
-			Item.damage = 16;
-			Item.DamageType = DamageClass.Melee;
-			Item.width = 40;
-			Item.height = 40;
-			Item.useTime = 40;
-			Item.scale = 1.35f;
-			Item.useAnimation = 40;
-			Item.useStyle = ItemUseStyleID.Swing;
-			Item.knockBack = 4;
-			Item.value = Item.buyPrice(silver: 1);
-			Item.rare = ItemRarityID.Blue;
-			Item.UseSound = SoundID.Item1;
-			Item.autoReuse = true;
-		}
+    public class BambooSword : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.damage = 16;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 40;
+            Item.height = 40;
+            Item.useTime = 40;
+            Item.scale = 1.35f;
+            Item.useAnimation = 40;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.knockBack = 4;
+            Item.value = Item.buyPrice(silver: 1);
+            Item.rare = ItemRarityID.Blue;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = true;
 
-		public override void AddRecipes()
-		{
-			Recipe recipe = CreateRecipe();
-			recipe.AddIngredient(ItemID.DirtBlock, 10);
-			recipe.AddTile(TileID.WorkBenches);
-			recipe.Register();
-		}
-	}
+            //shoot projectile (for animation)
+            Item.noUseGraphic = true;
+            Item.shoot = ModContent.ProjectileType<BambooSwordAni>();
+            Item.shootSpeed = 0f;
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.DirtBlock, 10);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
+    }
 }

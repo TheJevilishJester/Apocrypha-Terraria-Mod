bfcda1d0db5b24cacd7a3ca49736ff09d491b0f0
White space <alexgaming9983579898@gmail.com>
2026-08-26 18:40:28 -0400

    Update GelStrikerAni.cs Namespace

--- Content/Projectiles/GelStrikerAni.cs
+++ Content/Projectiles/GelStrikerAni.cs
@@ -4,7 +4,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace ApocryphaAnimatorBranch.Content.Projectiles
+namespace TheApocryphaMod.Content.Projectiles
 {
     public class GelStrikerAni : ModProjectile
     {

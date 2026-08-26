9747ac6e2473a23aeb4de081519a1d4e55242a00
White space <alexgaming9983579898@gmail.com>
2026-08-26 18:39:20 -0400

    Update GelStriker.cs (SwordAnimation)

--- Content/Items/Weapons/GelStriker/GelStriker.cs
+++ Content/Items/Weapons/GelStriker/GelStriker.cs
@@ -1,16 +1,17 @@
+using TheApocryphaMod.Content.Projectiles;
+using System.Collections.Generic;
 using Terraria;
 using Terraria.ID;
-using Terraria.ModLoader;
 using Terraria.Localization;
-using System.Collections.Generic;
+using Terraria.ModLoader;
 
 namespace TheApocryphaMod.Content.Items.Weapons.GelStriker
 {
-	// This is a basic item template.
-	// Please see tModLoader's ExampleMod for every other example:
-	// https://github.com/tModLoader/tModLoader/tree/stable/ExampleMod
-	public class GelStriker : ModItem
-	{
+    // This is a basic item template.
+    // Please see tModLoader's ExampleMod for every other example:
+    // https://github.com/tModLoader/tModLoader/tree/stable/ExampleMod
+    public class GelStriker : ModItem
+    {
         public override void OnHitNPC(Player player, NPC target, NPC.HitInfo hit, int damageDone)
         {
             target.AddBuff(BuffID.Slimed, 180);
@@ -18,28 +19,34 @@ namespace TheApocryphaMod.Content.Items.Weapons.GelStriker
 
         // The Display Name and Tooltip of this item can be edited in the 'Localization/en-US_Mods.TestingMod.hjson' file.
         public override void SetDefaults()
-		{
-			Item.damage = 30;
-			Item.DamageType = DamageClass.Melee;
-			Item.width = 80;
-			Item.height = 80;
-			Item.useTime = 30;
-			Item.scale = 1.8F;
-			Item.useAnimation = 30;
-			Item.useStyle = ItemUseStyleID.Swing;
-			Item.knockBack = 8;
-			Item.value = Item.buyPrice(silver: 1);
-			Item.rare = ItemRarityID.Blue;
-			Item.UseSound = SoundID.Item1;
-			Item.autoReuse = true;
-		}
+        {
+            Item.damage = 30;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 80;
+            Item.height = 80;
+            Item.useTime = 30;
+            Item.scale = 1.8F;
+            Item.useAnimation = 30;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.knockBack = 8;
+            Item.value = Item.buyPrice(silver: 1);
+            Item.rare = ItemRarityID.Blue;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = true;
 
-		public override void AddRecipes()
-		{
-			Recipe recipe = CreateRecipe();
-			recipe.AddIngredient(ItemID.StoneBlock, 10);
-			recipe.AddTile(TileID.WorkBenches);
-			recipe.Register();
-		}
-	}
+            //shoot projectile (for animation)
+            Item.noUseGraphic = true;
+            Item.shoot = ModContent.ProjectileType<GelStrikerAni>();
+            Item.shootSpeed = 0f;
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.StoneBlock, 10);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
+    }
+}   
 }
